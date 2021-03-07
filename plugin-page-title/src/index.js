63c2d170acbc4a1bd864c4c5f3c1bc9349fdfb0a
Duncan Faulkner <info@e-ips.com>
2021-03-07 17:30:49 +0000

    refactor(postprocessbyhtml): update the render process to postProcessByHtml
    update Scully to the latest version to get postProcessByHtml as render is now depricated
    
    BREAKING CHANGE: Update to lastest Scully version
    
    #8

--- plugin-page-title/src/index.js
+++ plugin-page-title/src/index.js
@@ -2,7 +2,7 @@ const { registerPlugin } = require('@scullyio/scully');
 const { changeTitlePlugin } = require('./plugin-page-title');
 
 const validator = async (config) => [];
-registerPlugin('render', 'changeTitlePlugin', changeTitlePlugin, validator);
+registerPlugin('postProcessByHtml', 'changeTitlePlugin', changeTitlePlugin, validator);
 
 module.exports = {
 	changeTitlePlugin,
--- scully/plugins/plugin.ts
+++ scully/plugins/plugin.ts
@@ -1,12 +1,11 @@
-
 import { registerPlugin, getPluginConfig } from '@scullyio/scully';
 
 export const myPlugin = 'myPlugin';
 
 const myFunctionPlugin = async (html: string): Promise<string> => {
-  return html;
+	return html;
 };
 
 const validator = async () => [];
 
-registerPlugin('render', myPlugin, myFunctionPlugin, validator);
+registerPlugin('postProcessByHtml', myPlugin, myFunctionPlugin, validator);
